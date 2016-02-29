bc2f4467c75aec98f0bc3db53a170e1bd6ace4c3
Angelo Luppino <angelo.luppino@spcapitaliq.com>
2016-02-28 20:45:47 -0500

    Add toolbar skeleton

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,16 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var exportButton: UIBarButtonItem!
+    @IBOutlet weak var trashButton: UIBarButtonItem!
+    
+    let hasCamera = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        exportButton.enabled = false
+        trashButton.enabled = false
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    @IBAction func tapCamera(sender: UIBarButtonItem) {
+        let alertController = UIAlertController()
+        let choosePhotoAction = UIAlertAction(title:"Photo Library", style:UIAlertActionStyle.Default) {
+            action in self.dismissViewControllerAnimated(true, completion: nil)
+            self.openCamera(false)
+        }
+        let takePhotoAction = UIAlertAction(title:"Take Photo", style:UIAlertActionStyle.Default) {
+            action in self.dismissViewControllerAnimated(true, completion: nil)
+            if (self.hasCamera) {
+                self.openCamera(true)
+            } else {
+                self.alertNoCamera()
+            }
+        }
+        let cancelAction = UIAlertAction(title:"Cancel", style:UIAlertActionStyle.Cancel) {
+            action in self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        alertController.addAction(choosePhotoAction)
+        alertController.addAction(takePhotoAction)
+        alertController.addAction(cancelAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func openCamera(takeNewPhoto: Bool) {
+        let imagePicker = UIImagePickerController()
+        if (takeNewPhoto) {
+            imagePicker.sourceType = .Camera
+        }
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func alertNoCamera() {
+        let alertController = UIAlertController()
+        alertController.title = "No camera detected"
+        let okAction = UIAlertAction(title:"OK", style: UIAlertActionStyle.Default) {
+            action in self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        alertController.addAction(okAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
 
 }
 
